253c3236483f4983803882313a78481f003b668b
Spookywooky3 <33356007+Spookywooky3@users.noreply.github.com>
2019-12-05 23:37:04 +0800

    Initial Commit

---
+++ GetUserIdFromUsername/DiscordApi.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace GetUserIdFromUsername
+{
+    // Shit naming conventions and kind of messy but cbf lmao
+    public class DiscordApi : IDisposable
+    {
+        private bool disposed = false;
+
+        string botToken { get; set; }
+        public DiscordApi(string token)
+        {
+            botToken = token;
+        }
+
+        public async Task<ulong> GetIdFromUserName(ulong guildId, string usr)
+        {
+            string[] userArray = usr.Split('#');
+            string username = userArray[0];
+            string discriminator = userArray[1];
+
+            string dataReceived;
+
+            // Change the limit depending on server members and performance, dodgey i know but its better than checking the amount of guild members
+            // every time.
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://discordapp.com/api/v6/guilds/" 
+                + guildId.ToString() + "/members?limit=1000"); 
+            req.PreAuthenticate = true;
+            req.Headers.Add("Authorization", "Bot " + botToken);
+            req.Method = "GET";
+
+            var response = await req.GetResponseAsync();
+            using (Stream data = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(data);
+                dataReceived = await reader.ReadToEndAsync();
+            }
+            response.Close();
+
+            var json = JsonConvert.DeserializeObject<List<Wrapper>>(dataReceived);
+            var query = json.Single(user => user.discordUser.userName == username && user.discordUser.discriminator == discriminator);
+
+            ulong userId;
+            ulong.TryParse(query.discordUser.id, out userId);
+
+            return userId;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+            if (disposing)
+            {
+                GetIdFromUserName(0, "").Dispose();
+            }
+            disposed = true;
+        }
+
+    }
+
+    class Wrapper
+    {
+        [JsonProperty("user")]
+        public DiscordUser discordUser { get; set; }
+        [JsonProperty("roles")]
+        public List<object> roleIds { get; set; }
+        [JsonProperty("nick")]
+        public object nick { get; set; }
+        [JsonProperty("joined_at")]
+        public DateTime joined_at { get; set; }
+        [JsonProperty("premium_since")]
+        public object premium_since { get; set; }
+        [JsonProperty("deaf")]
+        public bool deaf { get; set; }
+        [JsonProperty("mute")]
+        public bool mute { get; set; }
+    }
+
+    class DiscordUser
+    {
+        [JsonProperty("id")]
+        public string id { get; set; }
+        [JsonProperty("username")]
+        public string userName { get; set; }
+        [JsonProperty("avatar")]
+        public string avatar { get; set; }
+        [JsonProperty("discriminator")]
+        public string discriminator { get; set; }
+        [JsonProperty("bot")]
+        public bool bot { get; set; }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using GetUserIdFromUsername;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (DiscordApi api = new DiscordApi("<token for bot that is inside the server>"))
+            {
+                Console.WriteLine(api.GetIdFromUserName(564100801685880834, "user#1337").ConfigureAwait(false).GetAwaiter().GetResult().ToString());
+                // await api.GetIdFromUserName(guildId, "user#1337");   
+            }
+            Console.ReadKey();
+        }
+    }
+}
