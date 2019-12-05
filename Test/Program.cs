0525d039222ec906d5cf8c6924e1adafb386034a
Spookywooky3 <33356007+Spookywooky3@users.noreply.github.com>
2019-12-06 01:51:52 +0800

    PSA: Appveyor is shit when it comes to .NET Core 3.1

--- Test/Program.cs
+++ Test/Program.cs
@@ -9,7 +9,7 @@ namespace Test
         {
             using (DiscordApi api = new DiscordApi("<token for bot that is inside the server>"))
             {
-                Console.WriteLine(api.GetIdFromUserName(guildId, "user#1337")
+                Console.WriteLine(api.GetIdFromUserName(0, "user#1337")
                 .ConfigureAwait(false).GetAwaiter().GetResult().ToString());
 
                 // await api.GetIdFromUserName(guildId, "user#1337");   
